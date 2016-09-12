729e24d8dfc9715ff90ad16b973d1bf00fb039b6
Elie Zananiri <prisonerjohn@gmail.com>
2016-09-12 18:34:11 +0200

    Using position, orientation, and scale to serialize ofNode instead of transform matrix as it was less reliable. Added warning when deserializing ofNode using transform matrix.

--- src/ofxPreset/Serializer.h
+++ src/ofxPreset/Serializer.h
@@ -4,9 +4,9 @@
 
 namespace ofxPreset
 {
-    class Serializer
-    {
-    public:
+	class Serializer
+	{
+	public:
 		template<typename DataType>
 		static inline nlohmann::json & Serialize(nlohmann::json & json, const vector<DataType> & values, const string & name = "");
 		template<typename DataType>
@@ -15,8 +15,8 @@ namespace ofxPreset
 		static inline nlohmann::json & Serialize(nlohmann::json & json, const ofAbstractParameter & parameter);
 		static inline const nlohmann::json & Deserialize(const nlohmann::json & json, ofAbstractParameter & parameter);
 
-        static inline nlohmann::json & Serialize(nlohmann::json & json, const ofParameterGroup & group);
-        static inline const nlohmann::json & Deserialize(const nlohmann::json & json, ofParameterGroup & group);
+		static inline nlohmann::json & Serialize(nlohmann::json & json, const ofParameterGroup & group);
+		static inline const nlohmann::json & Deserialize(const nlohmann::json & json, ofParameterGroup & group);
 
 		static inline nlohmann::json & Serialize(nlohmann::json & json, const ofEasyCam & easyCam, const string & name = "");
 		static inline const nlohmann::json & Deserialize(const nlohmann::json & json, ofEasyCam & easyCam, const string & name = "");
@@ -24,8 +24,8 @@ namespace ofxPreset
 		static inline nlohmann::json & Serialize(nlohmann::json & json, const ofCamera & camera, const string & name = "");
 		static inline const nlohmann::json & Deserialize(const nlohmann::json & json, ofCamera & camera, const string & name = "");
 
-        static inline nlohmann::json & Serialize(nlohmann::json & json, const ofNode & node, const string & name = "");
-        static inline const nlohmann::json & Deserialize(const nlohmann::json & json, ofNode & node, const string & name = "");
+		static inline nlohmann::json & Serialize(nlohmann::json & json, const ofNode & node, const string & name = "");
+		static inline const nlohmann::json & Deserialize(const nlohmann::json & json, ofNode & node, const string & name = "");
 	};
 }
 
